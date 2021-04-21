f02735a73d9c9ab1ea248ab4dfcdae1a846cee01
anagodlevska <muqnas@gmail.com>
2021-04-21 12:55:01 +0300

    Register and Login page/CSS update for pages

--- client/src/Pages/AuthPage.jsx
+++ client/src/Pages/AuthPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import {Link} from "react-router-dom";
 
 
 function AuthPage () {
@@ -8,7 +9,26 @@ function AuthPage () {
             <Helmet>
                 <title>Login</title>
             </Helmet>
-            <h1>LOGIN PAGE</h1>
+            <div className="header bg-overlay"/>
+            <div className="all-screen">
+                <div className="main-box">
+                    <form className="formLogin" >
+                        <h1>Login</h1>
+                        <div className="text_field">
+                            <input type="email" id="email" name="email" required/>
+                            <span></span>
+                            <label htmlFor="email">E-mail</label>
+                        </div>
+                        <div className="text_field">
+                            <input type="password" id="pass" name="password" minLength="6" required/>
+                            <span></span>
+                            <label htmlFor="pass">Password</label>
+                        </div>
+                        <button type="submit">LOGIN</button>
+                        <Link to="/registration">Not a member? Sign in!</Link>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import {Link} from "react-router-dom";
 
 
 function RegisterPage () {
@@ -8,7 +9,36 @@ function RegisterPage () {
             <Helmet>
                 <title>Registration</title>
             </Helmet>
-            <h1>Registration PAGE</h1>
+            <div className="header bg-overlay"/>
+            <div className="all-screen">
+                <div className="main-box">
+                    <form className="formLogin" >
+                        <h1>Registration</h1>
+                        <div className="text_field" >
+                            <input type="tel" id="telephone" name="telephone" required/>
+                            <span></span>
+                            <label htmlFor="telephone">Telephone</label>
+                        </div>
+                        <div className="text_field">
+                            <input type="tel" id="name" name="name" required/>
+                            <span></span>
+                            <label htmlFor="name">Name</label>
+                        </div>
+                        <div className="text_field">
+                            <input type="email" id="email" name="email" required/>
+                            <span></span>
+                            <label htmlFor="email">E-mail</label>
+                        </div>
+                        <div className="text_field">
+                            <input type="password" id="pass" name="password" minLength="6" required/>
+                            <span></span>
+                            <label htmlFor="pass">Password</label>
+                        </div>
+                        <button type="submit">REGISTER</button>
+                        <Link to="/login">Already registered? Log in!</Link>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
