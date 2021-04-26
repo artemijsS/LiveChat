93303a0f87a38b93aaad6e39a2a317c29abb76a3
anagodlevska <muqnas@gmail.com>
2021-04-26 13:10:56 +0300

    New db model - message

---
+++ models/Message.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require('mongoose')
+
+const schema = new Schema({
+    text: {type: String, required: true},
+    owner: {type: Types.ObjectId, ref: 'User', required: true},
+    recipient: {type: Types.ObjectId, ref: 'User', required: true},
+    time: {type: String, required: true},
+    status: {type: Boolean, required: true},
+    dialog: {type: Types.ObjectId, ref: 'Dialog', required: true}
+})
+
+module.exports = model('Message', schema)
