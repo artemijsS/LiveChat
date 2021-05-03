85f8f2c1d73a4314af538407cb31ab2584639d72
anagodlevska <muqnas@gmail.com>
2021-05-03 12:15:59 +0300

    default value of last_message_created_at in Dialog Model

--- models/Dialog.js
+++ models/Dialog.js
@@ -5,7 +5,7 @@ const schema = new Schema({
     participant2_id: {type: Types.ObjectId, ref: 'User', required: true},
     last_message: {type: String},
     last_message_time: {type: String},
-    last_message_created_at: {type: Date},
+    last_message_created_at: {type: Date, default: Date.now},
     last_message_owner: {type: Types.ObjectId, ref: 'User'},
     last_message_status: {type: Boolean},
     messages: [{type: Types.ObjectId, ref: 'Message'}]
