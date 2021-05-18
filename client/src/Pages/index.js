2b44fa2146aa15b9dcd6ee66d66c85aaa4be5c7c
anagodlevska <muqnas@gmail.com>
2021-05-18 21:26:32 +0300

    Loading Page Update

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,3 +1,4 @@
 export {default as MainPage} from './MainPage'
 export {default as AuthPage} from './AuthPage'
 export {default as RegisterPage} from './RegisterPage'
+export {default as LoadingPage} from './LoadingPage'
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'
 
-import {MainPage, AuthPage, RegisterPage} from "./Pages";
+import {MainPage, AuthPage, RegisterPage, LoadingPage} from "./Pages";
 
 import {useSelector} from "react-redux";
-import LoadingPage from "./Pages/LoadingPage";
 
 
 export const useRoutes = () => {
