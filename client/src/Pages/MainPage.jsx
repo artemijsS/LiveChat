c69f728be152e40a1cc1be0a73538d3149333604
artemijsS <artjom1408@gmail.com>
2021-05-19 17:59:03 +0300

    settings popUp

--- client/src/Pages/MainPage.jsx
+++ client/src/Pages/MainPage.jsx
@@ -22,6 +22,7 @@ function MainPage () {
 
     const [activeFindNewDialog, setActiveFindNewDialog] = useState(false)
     const [messageText, setMessageText] = useState('')
+    const [settingsPopUp, setSettingsPopUp] = useState(false)
 
 
     useEffect(() => {
@@ -118,11 +119,20 @@ function MainPage () {
                                                 <path fill="currentColor"
                                                       d="M19.005 3.175H4.674C3.642 3.175 3 3.789 3 4.821V21.02l3.544-3.514h12.461c1.033 0 2.064-1.06 2.064-2.093V4.821c-.001-1.032-1.032-1.646-2.064-1.646zm-4.989 9.869H7.041V11.1h6.975v1.944zm3-4H7.041V7.1h9.975v1.944z"/>
                                             </svg>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                                            <svg onClick={() => {setSettingsPopUp(!settingsPopUp)}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                                                  height="24">
                                                 <path fill="currentColor"
                                                       d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"/>
                                             </svg>
+                                            {
+                                                settingsPopUp &&
+                                                <div className="settingsPopUp">
+                                                    <div className="settingsLink"><div>Profile</div></div>
+                                                    <div className="settingsLink"><div>Favourites</div></div>
+                                                    <div className="settingsLink"><div>Settings</div></div>
+                                                    <div className="settingsLink"><div>Logout</div></div>
+                                                </div>
+                                            }
                                         </div>
                                         {
                                             role === "admin" &&
